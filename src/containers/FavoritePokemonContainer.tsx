321a4be6b66158578459f3d4df442500127fa4fd
KevinHartono35 <khartono77@yahoo.com>
2022-02-04 03:44:54 +0700

    Fix Local Storage Error

--- src/containers/FavoritePokemonContainer.tsx
+++ src/containers/FavoritePokemonContainer.tsx
@@ -2,13 +2,15 @@ import React, {useEffect} from 'react';
 import {Container, Row} from 'react-bootstrap';
 
 export default function FavoritePokemonContainer() {
+  const local = localStorage.getItem('favorite-pokemon');
+
   return (
     <Container className="mb-5">
       <Row>
         <h1>Favorito</h1>
       </Row>
       <Row>
-        <h2>{JSON.parse(localStorage.getItem('favorite-pokemon') || '')?.name}</h2>
+        <h2>{local && JSON.parse(local).name}</h2>
       </Row>
     </Container>
   );
